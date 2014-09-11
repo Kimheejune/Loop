a7cd70da4d573a337b0d1f35134acf34b544bf9b
Kimheejune <qwqw0102@hanmail.net>
2014-09-11 15:59:18 +0900

    SDFSDFSDFDFRJHRTJRTJR

--- sketch.js
+++ sketch.js
@@ -1,24 +1,23 @@
 function setup() {
   createCanvas(640, 480);
-  background(128, 0, 64);
+  
   noFill();
 
   stroke(255, 0, 0);
 
-/*
-  var x;
+}
 
-  x = 1;
-  ellipse(x * 150, 100, 50, 50);
+function draw () {
 
-  x = 2;
-  ellipse(x * 150, 100, 50, 50);
+   background(128, 0, 64);
 
-  x = 3;
-  ellipse(x * 150, 100, 50, 50);
-  */
-   for (var x = 1; x <=8; x++)
-  {
-    ellipse(x * 70, x * 20 + 100, x + 50, x * 10);
-  }
+for (var x = 1; x <= 8; x++)
+{
+  ellipse(x * 70, x * 20 + 100, x+50, x*10);
+}
+
+var size =    Math.abs(mouseX - 320);
+
+ellipse(320, 240, mouseX, mouseY);
+
 }
